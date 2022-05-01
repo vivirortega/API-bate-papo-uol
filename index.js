2a3edd3f322078ef6d66019a75403c350a852577
unverzed <vitoriarortega@gmail.com>
2022-05-01 20:26:27 -0300

    feat: connection with database

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
+
 
 const app = express();
 app.use(cors());
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+mongoClient.connect().then(() => {
+    db = mongoClient.db("batepapo_uol");
+    console.log("Conectado ao banco de dados com sucesso")
+});
 
 app.listen(5000, () => console.log("Server working"));
