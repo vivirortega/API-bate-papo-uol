e73b67ae7da57e1f9fd7a6b93db3dfbe8420b699
unverzed <vitoriarortega@gmail.com>
2022-05-02 22:19:02 -0300

    feat: post for messages

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import dayjs from "dayjs";
 import joi from "joi";
-import { MongoClient } from "mongodb";
+import { MongoClient, MongoServerClosedError } from "mongodb";
 
 const app = express();
 app.use(cors());
@@ -60,13 +60,61 @@ app.post("/participants", async (req, res) => {
 });
 
 app.get("/participants", async (req, res) => {
-    try {
-      const users = await db.collection("participants").find().toArray();
-      res.send(users);
-    } catch (e) {
-      res.status(500).send("Não foi possível encontrar os usuários");
-      console.log("deu erro", e);
-    }
+  try {
+    const users = await db.collection("participants").find().toArray();
+    res.send(users);
+  } catch (e) {
+    res.status(500).send("Não foi possível encontrar os usuários");
+    console.log("deu erro", e);
+  }
+});
+
+app.post("/messages", async (req, res) => {
+  const { to, text, type } = req.body;
+  const user = req.headers.user;
+
+  const messageSchema = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.required().valid("message", "private_message"),
+    from: joi.string().required(),
   });
+
+  const validation = messageSchema.validate(
+    { to, text, type },
+    { abortEarly: true }
+  );
+
+  if (validation.error) {
+    res.status(422).send("Não foi possível enviar a mensagem");
+    return;
+  }
+
+  try {
+    const checkUser = await db
+      .collection("participants")
+      .find({ name: user })
+      .toArray();
+
+    if (!checkUser) {
+      res.sendStatus(422);
+      return;
+    }
+
+    const time = dayjs().format("HH:mm:ss");
+
+    await db.collection("messages").insertOne({
+      from: user,
+      to: to,
+      text: text,
+      type: type,
+      time: time,
+    });
+    res.sendStatus(201);
+  } catch (e) {
+    console.log(e);
+    res.status(500);
+  }
+});
 
 app.listen(5000, () => console.log("Server working"));
